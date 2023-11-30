9be23c8bdf45118bef7f14b516e4ac94cdb48a61
Matheus438 <matheusbispoa3@gmail.com>
2023-11-30 12:41:21 -0300

    commit do erro de validaçao

--- src/components/ListagemProfissionais.tsx
+++ src/components/ListagemProfissionais.tsx
@@ -144,7 +144,7 @@ const ListagemProfissionais = () => {
                                                 <td>{profissionais.cpf}</td>
 
                                                 <td>
-                                                <Link to={"/editar/Servico/"+ profissionais.id}  className='btn btn-primary btn-sm' >Editar</Link>
+                                                <Link to={"/profissional/editar/"+ profissionais.id}  className='btn btn-primary btn-sm' >Editar</Link>
                                                 <a onClick={e => handleDelete(profissionais.id)} className='btn m-1 btn-danger btn-sm'>Excluir</a>
                                                 <a onClick={e => RedefinirSenha(profissionais.id)} className='btn btn-secondary btn-sm'>Redefinir Senha</a>
                                                 </td>
